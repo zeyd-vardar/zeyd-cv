fe2eff27e56aa40d876bdede4cd403cf9896c736
Zeyd Vardar <zeydvardar61@gmail.com>
2026-09-18 17:47:03 +0300

    push edildi

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./",
+  base: "/zeyd-cv/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
